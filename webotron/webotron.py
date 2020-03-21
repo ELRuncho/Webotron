8860ea91d2dc4471f4ab94f9c75a6d42b6ed7b21
ElRuncho <francorafae@gmail.com>
2020-03-21 00:01:53 -0500

    new options

--- webotron/webotron.py
+++ webotron/webotron.py
@@ -3,12 +3,23 @@ import click
 session= boto3.Session(profile_name='sandbox')
 s3 = session.resource('s3')
 
-@click.command('list-buckets')
+@click.group()
+def cli():
+    'Webotron uploads websites to AWS S3'
+    pass
+
+@cli.command('list-buckets')
 def list_buckets():
     'Lists buckets'
     for bucket in s3.buckets.all():
         print(bucket)
 
+@cli.command('list-bucket-objects')
+@click.argument('bucket')
+def list_bucket_objects(bucket):
+    "List of objects on a specified S3 Bucket"
+    for obj in s3.Bucket(bucket).objects.all():
+        print(obj)
 
 if __name__ == "__main__":
-    list_buckets()
+    cli()
